767cc3762e9d11449702173faeeff7f9a6f3cfaf
Azat6393 <58897122+Azat6393@users.noreply.github.com>
2021-08-20 18:55:35 +0300

    Added nova and ecoscore images

--- app/src/main/java/com/azatberdimyradov/openfoodfacts/utils/Constants.kt
+++ app/src/main/java/com/azatberdimyradov/openfoodfacts/utils/Constants.kt
@@ -11,6 +11,17 @@ object Constants {
     const val NUTRISCORE_D = "d"
     const val NUTRISCORE_E = "e"
 
+    const val NOVA_1 = 1
+    const val NOVA_2 = 2
+    const val NOVA_3 = 3
+    const val NOVA_4 = 4
+
+    const val ECOSCORE_A = "a"
+    const val ECOSCORE_B = "b"
+    const val ECOSCORE_C = "c"
+    const val ECOSCORE_D = "d"
+    const val ECOSCORE_E = "e"
+
     const val SUCCESS_TIME_DELAY = 1500L
 
 }
--- app/src/main/java/com/azatberdimyradov/openfoodfacts/utils/Other.kt
+++ app/src/main/java/com/azatberdimyradov/openfoodfacts/utils/Other.kt
@@ -1,11 +1,24 @@
 package com.azatberdimyradov.openfoodfacts.utils
 
-import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import com.azatberdimyradov.openfoodfacts.R
 import com.azatberdimyradov.openfoodfacts.data.local.ProductItem
 import com.azatberdimyradov.openfoodfacts.data.remote.models.ProductResponse
+import com.azatberdimyradov.openfoodfacts.utils.Constants.ECOSCORE_A
+import com.azatberdimyradov.openfoodfacts.utils.Constants.ECOSCORE_B
+import com.azatberdimyradov.openfoodfacts.utils.Constants.ECOSCORE_C
+import com.azatberdimyradov.openfoodfacts.utils.Constants.ECOSCORE_D
+import com.azatberdimyradov.openfoodfacts.utils.Constants.ECOSCORE_E
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NOVA_1
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NOVA_2
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NOVA_3
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NOVA_4
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NUTRISCORE_A
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NUTRISCORE_B
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NUTRISCORE_C
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NUTRISCORE_D
+import com.azatberdimyradov.openfoodfacts.utils.Constants.NUTRISCORE_E
 import com.google.android.material.snackbar.Snackbar
 
 
@@ -26,13 +39,34 @@ fun showSnackBar(message: String?, view: View) {
     ).show()
 }
 
-fun ImageView.setNutriscoreImage(nutriscore: String){
+fun ImageView.setNutriscoreImage(nutriscore: String) {
     when (nutriscore) {
-        Constants.NUTRISCORE_A -> this.setBackgroundResource(R.drawable.ic_nutriscore_a)
-        Constants.NUTRISCORE_B -> this.setBackgroundResource(R.drawable.ic_nutriscore_b)
-        Constants.NUTRISCORE_C -> this.setBackgroundResource(R.drawable.ic_nutriscore_c)
-        Constants.NUTRISCORE_D -> this.setBackgroundResource(R.drawable.ic_nutriscore_d)
-        Constants.NUTRISCORE_E -> this.setBackgroundResource(R.drawable.ic_nutriscore_e)
+        NUTRISCORE_A -> this.setBackgroundResource(R.drawable.ic_nutriscore_a)
+        NUTRISCORE_B -> this.setBackgroundResource(R.drawable.ic_nutriscore_b)
+        NUTRISCORE_C -> this.setBackgroundResource(R.drawable.ic_nutriscore_c)
+        NUTRISCORE_D -> this.setBackgroundResource(R.drawable.ic_nutriscore_d)
+        NUTRISCORE_E -> this.setBackgroundResource(R.drawable.ic_nutriscore_e)
         else -> this.setBackgroundResource(R.drawable.ic_nutriscore_unknown)
+    }
+}
+
+fun ImageView.setNovaImage(nova: Int) {
+    when (nova) {
+        NOVA_1 -> this.setBackgroundResource(R.drawable.ic_nova_group_1)
+        NOVA_2 -> this.setBackgroundResource(R.drawable.ic_nova_group_2)
+        NOVA_3 -> this.setBackgroundResource(R.drawable.ic_nova_group_3)
+        NOVA_4 -> this.setBackgroundResource(R.drawable.ic_nova_group_4)
+        else -> this.setBackgroundResource(R.drawable.ic_nova_group_unknown)
+    }
+}
+
+fun ImageView.setEcoscoreImage(ecoscore: String) {
+    when (ecoscore) {
+        ECOSCORE_A -> this.setBackgroundResource(R.drawable.ic_ecoscore_a)
+        ECOSCORE_B -> this.setBackgroundResource(R.drawable.ic_ecoscore_b)
+        ECOSCORE_C -> this.setBackgroundResource(R.drawable.ic_ecoscore_c)
+        ECOSCORE_D -> this.setBackgroundResource(R.drawable.ic_ecoscore_d)
+        ECOSCORE_E -> this.setBackgroundResource(R.drawable.ic_ecoscore_e)
+        else -> this.setBackgroundResource(R.drawable.ic_ecoscore_unknown)
     }
 }
